dbd35e240b8c565a903d87122dbedd7f891fb3f2
Crowdbotics <team@crowdbotics.com>
2024-08-30 18:46:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quizzapp-49204",
+  url: "https://quizzapp-49204.botics.co",
+  api: "https://quizzapp-49204.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
