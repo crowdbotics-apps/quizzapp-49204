69ec6d1f7bc3a88cc997dbbfd369a22008f6bff5
Vadym Kolpak <vadym@tuluko.eu>
2024-08-30 21:49:38 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "quizzapp-49204",
-  url: "https://quizzapp-49204.botics.co",
-  api: "https://quizzapp-49204.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "quizzapp-49204",
+  "url": "https://quizzapp-49204.botics.co",
+  "api": "https://quizzapp-49204.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI2"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
